592b3f504f5a5aea243c07f4a5732e616c1ba660
JShaw <93174861+JShawGit@users.noreply.github.com>
2022-09-19 21:57:33 -0500

    Moved files. Updated paths. Added to README.

---
+++ scripts/graphing/learning_curve.py
@@ -0,0 +1,149 @@
+from matplotlib import pyplot as plt
+from termcolor import colored
+import pandas as pd
+import regex as re
+import get_config
+import os
+
+
+def lc_loss(path, data_frame, config, name):
+    """ Creates the model loss plot using the defined configurations and saves it in the results directory. """
+    # The current subject
+    subject_num = get_subject_name(name)
+
+    # Grabbing accuracy and val_accuracy data from passed dataframe
+    loss = data_frame['loss']
+    val_loss = data_frame['val_loss']
+
+    # Establishing the plot's colors
+    loss_line_color = config['loss_line_color']
+    val_loss_line_color = config['val_loss_line_color']
+
+    # Establishing the plot's font and font sizes
+    font_family = config['font_family']
+    label_font_size = config['label_font_size']
+    title_font_size = config['title_font_size']
+
+    # Setting the previously established parameters
+    plt.rcParams['font.family'] = font_family
+    plt.rc('axes', labelsize=label_font_size)
+    plt.rc('axes', titlesize=title_font_size)
+
+    # Creating plot
+    plt.plot(loss, color=loss_line_color)
+    plt.plot(val_loss, color=val_loss_line_color)
+    plt.title(subject_num.upper() + ' learning curve')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='best')
+
+    # Removing top and right axis lines
+    plt.rcParams['axes.spines.right'] = False
+    plt.rcParams['axes.spines.top'] = False
+
+    # Saving plot to results directory and closing figure to avoid further plotting
+    save_format = config['save_format']
+    save_res = config['save_resolution']
+
+    # Save the figure
+    plt.savefig(os.path.join(path, name) + '_lc_loss' + '.' + save_format, format=save_format, dpi=save_res)
+    plt.close()
+    print(colored("Loss Learning curve has been created for: " + name + "\n", 'green'))
+
+
+def lc_accuracy(path, data_frame, config, name):
+    """ Creates the model accuracy plot using the defined configurations and saves it in the results directory.  """
+    # The current subject
+    subject_num = get_subject_name(name)
+
+    # Grabbing accuracy and val_accuracy data from passed dataframe
+    acc = data_frame['accuracy']
+    val_acc = data_frame['val_accuracy']
+
+    # Establishing the plot's colors
+    acc_line_color = config['acc_line_color']
+    val_acc_line_color = config['val_acc_line_color']
+
+    # Establishing the plot's font and font sizes
+    font_family = config['font_family']
+    label_font_size = config['label_font_size']
+    title_font_size = config['title_font_size']
+
+    # Setting the previously established parameters
+    plt.rcParams['font.family'] = font_family
+    plt.rc('axes', labelsize=label_font_size)
+    plt.rc('axes', titlesize=title_font_size)
+
+    # Creating plot
+    plt.plot(acc, color=acc_line_color)
+    plt.plot(val_acc, color=val_acc_line_color)
+    plt.title(subject_num.upper() + ' learning curve')
+    plt.ylabel('accuracy', )
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='best')
+
+    # Saving plot to results directory and closing plot
+    save_format = config['save_format']
+    save_res = config['save_resolution']
+    plt.savefig(os.path.join(path, name) + '_lc_accuracy' + '.' + save_format, format=save_format, dpi=save_res)
+    plt.close()
+    print(colored("Accuracy Learning curve has been created for: " + name, 'green'))
+
+
+def get_subject_name(file_name):
+    """ Gets the subject id """
+    try:
+        subject_search = re.search('_test_.*_val_', file_name)
+        subject_name = subject_search.captures()[0].split("_")[2]
+        return subject_name
+    except:
+        raise Exception(colored(f"Error: File name doesn't contain '*_test_*_val_*' format: \n\t{file_name}", 'red'))
+
+
+def create_graphs(file_list, file_path, results_path, results_config):
+    """ Create graphs for each item """
+    # Looping through each file and creating needed graphs
+    for file in file_list:
+        # Reading in CSV file into a dataframe
+        results_df = pd.read_csv(os.path.join(file_path, file), index_col=0)
+        file_name = re.sub('.csv', '', file)
+
+        # Creating accuracy and loss plots
+        lc_accuracy(results_path, results_df, results_config, file_name)
+        lc_loss(results_path, results_df, results_config, file_name)
+
+
+def file_verification(files_list):
+    """ Verifies files are valid """
+    verified_list = []
+    for file in files_list:
+        history_check = re.search("history", file)
+        if history_check != None:
+            verified_list.append(file)
+        else:
+            pass
+    return verified_list
+
+
+def main(config=None):
+    """ The main program """
+    # Obtaining dictionary of configurations from json file
+    if config is None:
+        config = get_config.parse_json('learning_curve_config.json')
+
+    # Grab file directories 
+    file_path = config['input_path']
+    results_path = config['output_path']
+    results_exist = os.path.exists(results_path)
+    if not results_exist:
+        os.makedirs(results_path)
+
+    # Create list of files needed to process and graph them
+    list_files = os.listdir(file_path)
+    verified_list_files = file_verification(list_files)
+    create_graphs(verified_list_files, file_path, results_path, config)
+
+
+if __name__ == "__main__":
+    """ Executes the program """
+    main()
---
+++ scripts/graphing/roc_curve.py
@@ -0,0 +1,144 @@
+from sklearn.preprocessing import label_binarize
+from sklearn.metrics import roc_curve, auc
+from matplotlib import pyplot as plt
+from termcolor import colored
+from itertools import cycle
+import pandas as pd
+import numpy as np
+import get_config
+import os
+
+
+# TODO: change title to include file name
+
+
+def create_roc_curve(true_vals, pred_vals, roc_config, file_name, output_path):
+    """ Creates the ROC Graph. """
+    # Assuming true_vals is a numpy array
+    classes = np.unique(true_vals)
+    n_classes = len(classes)
+
+    # Binarize the labels
+    true_val_bin = label_binarize(true_vals, classes=classes)
+
+    # Create dictionaries for true pos, false pos, and roc values
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+
+    # For each class-index, generate values
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(true_val_bin[:, i], pred_vals[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # Generate values for a ravelled array
+    print(colored(f"File: {file_name}", 'blue'))
+    print(colored(f"True: {len(true_val_bin.ravel())}", 'blue'))
+    print(colored(f"Pred: {len(pred_vals.ravel())}", 'blue'))
+    fpr["micro"], tpr["micro"], _ = roc_curve(true_val_bin.ravel(), pred_vals.ravel())  # TODO: error line
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+    # First aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+    # Then interpolate all ROC curves at these points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += np.interp(all_fpr, fpr[i], tpr[i])
+
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+
+    fpr["macro"] = all_fpr
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+    lw = roc_config['line_width']
+    l_types = roc_config['label_types']
+    colors = cycle(roc_config['line_colors'])
+    save_format = roc_config['save_format']
+    save_res = roc_config['save_resolution']
+
+    # Establishing the plot's font and font sizes
+    font_family = roc_config['font_family']
+    label_font_size = roc_config['label_font_size']
+    title_font_size = roc_config['title_font_size']
+
+    # Setting the previously established parameters
+    plt.rcParams['font.family'] = font_family
+    plt.rc('axes', labelsize=label_font_size)
+    plt.rc('axes', titlesize=title_font_size)
+
+    # Removing top and right axis lines
+    plt.rcParams['axes.spines.right'] = False
+    plt.rcParams['axes.spines.top'] = False
+
+    # Create the plot
+    for i, type_name, color in zip(classes, l_types, colors):
+        plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+                 label='{0} (AUC = {1:0.2f})'
+                       ''.format(type_name, roc_auc[i]))
+    plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('1 - Specificity')
+    plt.ylabel('Sensitivity')
+    plt.title('Receiver Operating Characteristic(ROC): ' + file_name)
+    plt.legend(loc="best")
+
+    # Save the figure
+    plt.savefig(os.path.join(output_path, file_name) + '_roc_curve.' + save_format, dpi=save_res)
+    plt.close()
+
+
+def get_data(pred_path, true_path):
+    """ Reads in the labels and predictions from CSV. """
+    # Read CSV file
+    pred = pd.read_csv(pred_path, header=None).to_numpy()
+    true = pd.read_csv(true_path, header=None).to_numpy()
+
+    # Get shapes
+    pred_rows, pred_cols = pred.shape
+    true_rows, true_cols = true.shape
+
+    # Make the number of rows equal, in case uneven [[ TODO: Keep in with official data? ]]
+    if pred_rows > true_rows:
+        pred = pred[:true_rows, :]
+    elif pred_rows < true_rows:
+        true = true[:pred_rows, :]
+
+    # Return true and predicted values
+    return true, pred
+
+
+def main(config=None):
+    """ The main program. """
+
+    # Obtaining dictionary of configurations from json file
+    if config is None:
+        config = get_config.parse_json('roc_curve_config.json')
+
+    # Check that the output path exists
+    output_path = config['output_path']
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    # Obtain needed labels and predictions
+    pred_path = config['pred_path']
+    true_path = config['true_path']
+    if not os.path.exists(pred_path):
+        raise Exception(colored("Error: The prediction path is not valid!: " + pred_path, 'red'))
+    if not os.path.exists(true_path):
+        raise Exception(colored("Error: The true-value path is not valid!: " + true_path, 'red'))
+    true_val, pred_val = get_data(pred_path, true_path)
+    print(colored(true_path.split('/')[-1], 'yellow'))
+    print(colored(pred_path.split('/')[-1], 'yellow'))
+
+    # Create the curve
+    create_roc_curve(true_val, pred_val, config, config['output_file_prefix'], output_path)
+    print(colored("ROC curve created for: " + config['output_file_prefix'], "green"))
+
+
+if __name__ == "__main__":
+    """ Executes the program """
+    main()
