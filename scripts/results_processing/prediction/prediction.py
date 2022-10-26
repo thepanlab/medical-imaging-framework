eb9c2188f8fa0f69f202176ab9052b616bc9632a
JShaw <93174861+JShawGit@users.noreply.github.com>
2022-10-25 19:36:35 -0500

    Added an updated prediction

--- scripts/results_processing/prediction/prediction.py
+++ scripts/results_processing/prediction/prediction.py
@@ -3,8 +3,6 @@ from training import image_parser
 from termcolor import colored
 from tensorflow import keras
 import pandas as pd
-import argparse
-import json
 import time
 import os
 
@@ -19,11 +17,7 @@ def get_images(data_input, class_names):
 
         data_input: A dictionary of key-array form.
         return: A dictionary of key-array form, of all the images.
-    """
-    # To sort the input filenames by their index
-    def sort_condition(filename):
-        return int(filename.split('/')[-1].split('_')[0])
-                
+    """                
     # Get all valid image paths
     image_paths = {}
     for subject in data_input:     
@@ -35,7 +29,7 @@ def get_images(data_input, class_names):
             print(colored(f"Warning: The data path '{data_input[subject]}' is empty.", 'yellow'))
                 
         # Get all of the subject's image subdirectories
-        image_paths[subject] = {}
+        image_paths[subject] = []
         for dir in subdirs:
             dir = os.path.join(data_input[subject], dir)
             if not os.path.isdir(dir):
@@ -44,18 +38,11 @@ def get_images(data_input, class_names):
             if len(subfiles) == 0:
                 print(colored(f"Warning: The data path '{dir}' is empty.", 'yellow'))
                 continue
-            
-            # Iterate through every image path
-            img_class = [c for c in class_names if c in dir.split('/')[-1]]
-            if len(img_class) != 1:
-                raise Exception(colored(f"Error: Expected '{dir}' to contain one class name. It had {len(img_class)}.", 'red'))
-            image_paths[subject][img_class[0]] = []
             for file in subfiles:
                 if file.endswith((".png", ".jpg", ".jpeg")):
-                    image_paths[subject][img_class[0]].append(os.path.abspath(os.path.join(dir, file)))
+                    image_paths[subject].append(os.path.abspath(os.path.join(dir, file)))
                 else:
-                    print(colored(f"Warning: Non-png or non-jpg file detected. '{file}'", 'yellow'))  
-            image_paths[subject][img_class[0]].sort(key=sort_condition)  
+                    print(colored(f"Warning: Non-png or non-jpg file detected. '{file}'", 'yellow'))
     return image_paths
         
 
@@ -99,12 +86,10 @@ def predict_images(image_paths, models, batch_size, class_names, image_config):
     
     # Get label position
     for subject in image_paths:
-        for label in image_paths[subject]:
-            for file_name in image_paths[subject][label]:
-                labels = [class_name for class_name in class_names if class_name in file_name]
-                temp = os.path.abspath(file_name).split('.')
-                label_position = temp[0].split('_').index(labels[0])
-                break
+        for file_name in image_paths[subject]:
+            labels = [class_name for class_name in class_names if class_name in file_name]
+            temp = os.path.abspath(file_name).split('.')
+            label_position = temp[0].split('_').index(labels[0])
             break
         break
     
@@ -115,33 +100,32 @@ def predict_images(image_paths, models, batch_size, class_names, image_config):
         prediction_results[model] = {}
         timing_results[model] = {}
         
-        # Loop every subjects' label
+        # Loop through every subject
         for subject in image_paths:
             prediction_results[model][subject] = {}
             timing_results[model][subject] = {}
-            for label in image_paths[subject]:
                 
-                # Separate the images into batches
-                img_slices = tf.data.Dataset.from_tensor_slices(image_paths[subject][label])
-                img_map = img_slices.map(lambda x:image_parser.parse_image(
-                    x,  mean, use_mean, class_names, label_position, channels, 
-                    do_cropping,offset_height, offset_width, target_height, target_width
-                ))
-                img_batch = img_map.batch(batch_size, drop_remainder=False)
+            # Separate the images into batches
+            img_slices = tf.data.Dataset.from_tensor_slices(image_paths[subject])
+            img_map = img_slices.map(lambda x:image_parser.parse_image(
+                x,  mean, use_mean, class_names, label_position, channels, 
+                do_cropping,offset_height, offset_width, target_height, target_width
+            ))
+            img_batch = img_map.batch(batch_size, drop_remainder=False)
 
-                # Get the computation time
-                srt = time.time()
-                pred = models[model].predict(img_batch)
-                timing_results[model][subject][label] = time.time() - srt
-                
-                # Add predictions to the array
-                prediction_results[model][subject][label] = pred
-                print(colored(f"Successfully computed the predictions for: model '{model}' and input '{subject}' and label '{label}'", 'green'))
+            # Get the computation time
+            srt = time.time()
+            pred = models[model].predict(img_batch)
+            timing_results[model][subject] = time.time() - srt
+            
+            # Add predictions to the array
+            prediction_results[model][subject] = pred
+            print(colored(f"Successfully computed the predictions for: model '{model}' and input '{subject}'", 'green'))
         print(' ')
-    return prediction_results, timing_results
+    return prediction_results, timing_results, label_position
 
 
-def output_results(prediction_results, timing_results, input_filepaths, class_names, out_vals, out_mets):
+def output_results(prediction_results, timing_results, label_pos, input_filepaths, class_names, out_vals, out_mets):
     """
         Output the prediction results to CSV file.
 
@@ -154,76 +138,76 @@ def output_results(prediction_results, timing_results, input_filepaths, class_na
     for model in prediction_results:
         predicted_labels = pd.DataFrame(columns=columns_predicted_labels)
         for subject in prediction_results[model]:
-            for label in prediction_results[model][subject]:
                 
-                # The files will stored in a particular context
-                model_i = list(prediction_results.keys()).index(model)+1
-                subject_i = list(prediction_results[model].keys()).index(subject)
-                dirpath = os.path.join(
-                    out_vals,
-                    f"Test_subject_{subject}/config_{model_i}_{model}/{model}_{subject_i}_test_{subject}_val_{label}/"
-                )
-                print(colored(f"For model '{model}' and input '{subject}':", 'magenta'))
+            # The files will stored in a particular context
+            model_i = list(prediction_results.keys()).index(model)+1
+            subject_i = list(prediction_results[model].keys()).index(subject)
+            dirpath = os.path.join(
+                out_vals,
+                f"Test_subject_{subject}/config_{model_i}_{model}/{model}_{subject_i}_test_{subject}/"
+            )
+            print(colored(f"For model '{model}' and input '{subject}':", 'magenta'))
+            
+            # Make output directories
+            if not os.path.exists(os.path.join(dirpath, 'prediction')): os.makedirs(os.path.join(dirpath, 'prediction'))
+            if not os.path.exists(os.path.join(dirpath, 'true_label')): os.makedirs(os.path.join(dirpath, 'true_label'))
+            if not os.path.exists(os.path.join(dirpath, 'file_name')): os.makedirs(os.path.join(dirpath, 'file_name'))
+            prefix = f"{model}_{model_i}_test_{subject}_val"
                 
-                # Make output directories
-                if not os.path.exists(os.path.join(dirpath, 'prediction')): os.makedirs(os.path.join(dirpath, 'prediction'))
-                if not os.path.exists(os.path.join(dirpath, 'true_label')): os.makedirs(os.path.join(dirpath, 'true_label'))
-                if not os.path.exists(os.path.join(dirpath, 'file_name')): os.makedirs(os.path.join(dirpath, 'file_name'))
-                prefix = f"{model}_{model_i}_test_{subject}_val_{label}_val"
-                
-                # Print the prediction probability values
-                preds = pd.DataFrame(prediction_results[model][subject][label])
-                filename = os.path.join(dirpath, f"prediction/{prefix}_predicted.csv")
-                preds.to_csv(filename, index=False, header=False)
-                print(colored(f"\t Wrote the predictions.", 'cyan'))
-                
-                # Print the prediction index values
-                preds = preds.idxmax(axis=1)
-                filename = os.path.join(dirpath, f"prediction/{prefix}_predicted_index.csv")
-                preds.to_csv(filename, index=False, header=False)
-                print(colored(f"\t Wrote the indexed predictions.", 'cyan'))
-                
-                # Print the true labels
-                trues = pd.DataFrame([label]*len(input_filepaths[subject][label]))
-                filename = os.path.join(dirpath, f"true_label/{prefix}_true_label.csv")
-                trues.to_csv(filename, index=False, header=False)
-                print(colored(f"\t Wrote the true values.", 'cyan'))
-                
-                # Print the true label index values
-                trues = pd.DataFrame([class_names.index(label)]*len(input_filepaths[subject][label]))
-                filename = os.path.join(dirpath, f"true_label/{prefix}_true_label_index.csv")
-                trues.to_csv(filename, index=False, header=False)
-                print(colored(f"\t Wrote the indexed true values.", 'cyan'))
-                
-                # Print the true label index values
-                files = pd.DataFrame(input_filepaths[subject][label])
-                filename = os.path.join(dirpath, f"file_name/{prefix}_file.csv")
-                files.to_csv(filename, index=False, header=False)
-                print(colored(f"\t Wrote the image file paths.", 'cyan'))
-                
-                # Print the true label index values
-                time = pd.DataFrame([timing_results[model][subject][label]])
-                filename = os.path.join(dirpath, f"{prefix}_time_total.csv")
-                time.to_csv(filename, index=False, header=False)
-                print(colored(f"\t Wrote the timing.", 'cyan'))
-                
-                # Get the prediction info for every image
-                for i in range(len(input_filepaths[subject][label])):
-                    img_file = input_filepaths[subject][label][i].split('/')[-1]
-                    predictions = list(prediction_results[model][subject][label][i])
-                    max_index = predictions.index(max(predictions))
-                    max_label = class_names[max_index]
-                    predicted_labels = pd.concat([predicted_labels, pd.DataFrame.from_dict({
-                        'subject': [subject],
-                        'filename': [img_file],
-                        'true_label': [label],
-                        'true_label_index': [class_names.index(label)],
-                        'pred_label': [max_label],
-                        'pred_label_index': [max_index],
-                        'match': [class_names.index(label) == max_index]                        
-                    })], ignore_index=True)
-                print(colored(f"\t Got the prediction info.", 'cyan'))
-                print(colored(f"\t Finished writing the predictions.", 'green'))
+            # Print the prediction probability values
+            preds = pd.DataFrame(prediction_results[model][subject])
+            filename = os.path.join(dirpath, f"prediction/{prefix}_predicted.csv")
+            preds.to_csv(filename, index=False, header=False)
+            print(colored(f"\t Wrote the predictions.", 'cyan'))
+            
+            # Print the prediction index values
+            preds = preds.idxmax(axis=1)
+            filename = os.path.join(dirpath, f"prediction/{prefix}_predicted_index.csv")
+            preds.to_csv(filename, index=False, header=False)
+            print(colored(f"\t Wrote the indexed predictions.", 'cyan'))
+            
+            # Print the true labels
+            trues = pd.DataFrame(f.split('/')[-1].split('_')[3] for f in input_filepaths[subject])
+            filename = os.path.join(dirpath, f"true_label/{prefix}_true_label.csv")
+            trues.to_csv(filename, index=False, header=False)
+            print(colored(f"\t Wrote the true values.", 'cyan'))
+            
+            # Print the true label index values
+            trues = pd.DataFrame(class_names.index(label[0]) for label in trues.values)
+            filename = os.path.join(dirpath, f"true_label/{prefix}_true_label_index.csv")
+            trues.to_csv(filename, index=False, header=False)
+            print(colored(f"\t Wrote the indexed true values.", 'cyan'))
+            
+            # Print the true label index values
+            files = pd.DataFrame(input_filepaths[subject])
+            filename = os.path.join(dirpath, f"file_name/{prefix}_file.csv")
+            files.to_csv(filename, index=False, header=False)
+            print(colored(f"\t Wrote the image file paths.", 'cyan'))
+            
+            # Print the true label index values
+            time = pd.DataFrame([timing_results[model][subject]])
+            filename = os.path.join(dirpath, f"{prefix}_time_total.csv")
+            time.to_csv(filename, index=False, header=False)
+            print(colored(f"\t Wrote the timing.", 'cyan'))
+            
+            # Get the prediction info for every image
+            for i in range(len(input_filepaths[subject])):
+                img_file = input_filepaths[subject][i].split('/')[-1]
+                predictions = list(prediction_results[model][subject][i])
+                max_index = predictions.index(max(predictions))
+                max_label = class_names[max_index]
+                label = trues.values[i][0]
+                predicted_labels = pd.concat([predicted_labels, pd.DataFrame.from_dict({
+                    'subject': [subject],
+                    'filename': [img_file],
+                    'true_label': [label],
+                    'true_label_index': [class_names[label]],
+                    'pred_label': [max_label],
+                    'pred_label_index': [max_index],
+                    'match': [class_names[label] == max_index]                        
+                })], ignore_index=True)
+            print(colored(f"\t Got the prediction info.", 'cyan'))
+            print(colored(f"\t Finished writing the predictions.", 'green'))
         
         # Print the predicted labels
         filename = os.path.join(out_mets, f"{model}_prediction_info.csv")
@@ -258,10 +242,10 @@ def main(config=None):
     print(colored('Input models sucessfully read.\n', 'green'))
 
     # Predict the images
-    prediction_results, timing_results = predict_images(data, models, config['batch_size'], class_names, config["image_settings"])
+    prediction_results, timing_results, label_pos = predict_images(data, models, config['batch_size'], class_names, config["image_settings"])
 
     # Output the results
-    output_results(prediction_results, timing_results, data, class_names, out_vals, out_mets)
+    output_results(prediction_results, timing_results, label_pos, data, class_names, out_vals, out_mets)
     
 
 if __name__ == "__main__":
