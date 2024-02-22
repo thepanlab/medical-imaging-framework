0f002636ba8d79c82fd06c54d8b655a8a1cb310c
Paul Calle <pcallec@ou.edu>
2024-02-22 16:03:23 -0600

    update summary table for outer loop

--- scripts/results_processing/summary_table/summary_table.py
+++ scripts/results_processing/summary_table/summary_table.py
@@ -1,12 +1,17 @@
+import os
+import math
+import re
+from pathlib import Path
+import pandas as pd
 from sklearn.metrics import balanced_accuracy_score as weighted_acc
 from sklearn.metrics import accuracy_score as unweighted_acc
 from sklearn.metrics import f1_score as f1
 from util.get_config import parse_json
+from util.predicted_formatter.predicted_formatter import main as reformat
+from util.predicted_formatter.predicted_formatter import translate_file
+from util.predicted_formatter.predicted_formatter import write_file
 from termcolor import colored
 from util import path_getter
-import pandas as pd
-import math
-import os
 
 
 def read_data(paths):
@@ -45,6 +50,34 @@ def read_data(paths):
                     results[config][test_fold][val_fold] = pd.read_csv(validation_fold_path, header=None).to_numpy()
                 except:
                     print(colored(f"Warning: {validation_fold_path} is empty.", 'yellow'))
+
+    # Return the dictionary
+    return results
+
+def read_data_outer(l_paths):
+    """ From a list of paths of files with indices, it gets 
+    a dictionary where the key is the test fold and
+    the value is a numpy array
+
+    Args:
+        l_paths (list): list of paths of files with indices
+
+    Returns:
+        dict: A dictionary with key: test fold and value: numpy array.
+    """
+    # Return this
+    results = {}
+
+    # example of filenames
+    # resnet_50_0_test_fold8_label_index.csv
+    # resnet_50_test_E1_test-true-label-index.csv
+    for path in l_paths:
+        m = re.search('(?<=test_)[a-zA-Z0-9-]+', path.stem)
+        test_name = m.group(0)
+
+        a_value_index = pd.read_csv(path, header=None).to_numpy()
+
+        results[test_name] = a_value_index
 
     # Return the dictionary
     return results
@@ -172,7 +205,105 @@ def total_output(accuracies, standard_error, output_path, output_file, round_to,
         else:
             df.round(round_to).to_csv(f'{output_path}/{output_file}_inner_{key}.csv')
 
+def get_list_true_and_prediction_outer(config):
+    """ Based on the 'data_path' parameter in the config.
+    It returns list of path of true value indices and 
+    list of path of prediction indices
+    Args:
+        config (dict): A JSON configuration as a dictionary
 
+    Returns:
+        Two lists: 
+        * list with paths containing true value indices.
+        * list with paths containing prediction indices.
+
+    """
+
+    path_data = Path(config['data_path'])
+    
+    # Search for true_label files with index
+    l_true_label_index = list(path_data.rglob("*/true_label/*test_*index*"))
+    
+    if len(l_true_label_index) == 0:
+        raise RuntimeError(f"config['data_path'] doesn't contain folder(s) " + 
+                            "with name true_label that contain files with index")
+    
+    # Search for prediction
+    l_prediction = list(path_data.rglob("*/prediction/"))
+    
+    for prediction_folder in l_prediction:
+        l_files = list(prediction_folder.glob("*"))
+        if len(l_files) == 1:
+            translated_data = translate_file(l_files[0])
+            write_file(l_files[0].as_posix(), translated_data)
+            print(colored(f"Creating index file for {prediction_folder}", "green"))            
+            
+    l_prediction_index = list(path_data.rglob("*/prediction/*test_*index*"))
+
+    return l_true_label_index, l_prediction_index
+
+
+def save_dataframe_outer(dict_true_index, dict_prediction_index, config):
+    """ 
+    Based on the dictionaries it calculates: accuracy, weighted accuracy
+    and f1 weighted per test fold. Then, it creates a pandas dataframe 
+    and saves it as csv in 'output_path' and 'output_filename' as prefix.
+    
+    Args:
+        dict_true_index (dict): 
+        dict_prediction_index (dict): 
+        config (dict): A JSON configuration as a dictionary
+
+    """
+    l_test_folds = dict_true_index.keys()
+        
+    # TODO
+    # calculate accuracy and other metrics
+    dict_metrics = {'accuracy': unweighted_acc, 'weighted_accuracy': weighted_acc, 'f1_weighted': f1}
+    
+    df_all = pd.DataFrame()
+    
+    for test_fold in l_test_folds:
+        dict_row = {"test_fold":[test_fold]}
+        for metric in dict_metrics:
+            
+            true_vals = dict_true_index[test_fold]
+            pred_vals = dict_prediction_index[test_fold]
+            
+            if metric == 'f1_weighted':
+                metric_value = dict_metrics[metric](y_true=true_vals,
+                                    y_pred=pred_vals, average='weighted')
+            else:
+                metric_value = dict_metrics[metric](y_true=true_vals,
+                                                    y_pred=pred_vals)
+                
+            dict_row[metric] = metric_value
+
+        df_temp = pd.DataFrame(dict_row)
+        # join them in pandas dataframe 
+        df_all = pd.concat([df_all, df_temp], ignore_index=True)
+
+    p_path_folder_output = Path(config["output_path"])
+
+    p_path_folder_output.mkdir(parents=True, exist_ok=True)
+    
+    prefix_output = config["output_filename"]
+    
+    name_file = f"{prefix_output}_outer.csv"
+    
+    path_fileoutput = p_path_folder_output / name_file
+
+    df_all = df_all.sort_values("test_fold").reset_index(drop=True)
+    df_all = df_all.set_index('test_fold')
+    n_testfolds = df_all.shape[0]
+    
+    df_all.loc['mean'] = df_all.mean()
+    df_all.loc['std_err'] = df_all.std()/n_testfolds**0.5
+
+    df_all.to_csv(path_fileoutput)
+    print(colored(f"summary results for outer saved in: {path_fileoutput}.", 'green'))
+    # and save
+    
 def main(config=None):
     """ The main program.
 
@@ -183,21 +314,33 @@ def main(config=None):
     if config is None:
         config = parse_json('./results_processing/summary_table/summary_table_config.json')
 
-    # Get the necessary input files
-    true_paths = path_getter.get_subfolder_files(config['data_path'], "true_label", isIndex=True, getValidation=True, isOuter=config['is_outer'])
-    pred_paths = path_getter.get_subfolder_files(config['data_path'], "prediction", isIndex=True, getValidation=True, isOuter=config['is_outer'])
+    if config["is_outer"] == False:
+        
+        # Get the necessary input files
+        true_paths = path_getter.get_subfolder_files(config['data_path'], "true_label", isIndex=True, getValidation=True, isOuter=config['is_outer'])
+        pred_paths = path_getter.get_subfolder_files(config['data_path'], "prediction", isIndex=True, getValidation=True, isOuter=config['is_outer'])
 
-    # Read in each file into a dictionary
-    true = read_data(true_paths)
-    pred = read_data(pred_paths)
+        # Read in each file into a dictionary
+        true = read_data(true_paths)
+        pred = read_data(pred_paths)
+        
+        # Get accuracies and standard error
+        accuracies, stderr = get_accuracies_and_stderr(true, pred)
+
+        # Graph results
+        total_output(accuracies, stderr, config['output_path'], config['output_filename'], config['round_to'], is_outer=config['is_outer'])
+        print(colored("Finished writing the summary tables.", 'green'))
     
-    # Get accuracies and standard error
-    accuracies, stderr = get_accuracies_and_stderr(true, pred)
+    # config["is_outer"] == False
+    else:
 
-    # Graph results
-    total_output(accuracies, stderr, config['output_path'], config['output_filename'], config['round_to'], is_outer=config['is_outer'])
-    print(colored("Finished writing the summary tables.", 'green'))
+        l_true_label_index, l_prediction_index = get_list_true_and_prediction_outer(config)
 
+        dict_true_label_index = read_data_outer(l_true_label_index)
+        dict_prediction_index= read_data_outer(l_prediction_index)
+
+        save_dataframe_outer(dict_true_label_index, dict_prediction_index, config)
+        
 
 if __name__ == "__main__":
     """ Executes the program """
