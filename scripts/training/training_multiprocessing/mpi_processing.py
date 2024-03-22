caf1fd98c6f94b3d5e2c5db5f8d7ee96f24778e8
Paul Calle <pcallec@ou.edu>
2024-03-22 17:12:17 -0500

    Modify mpi_processing with perf_counter and other updates

--- scripts/training/training_multiprocessing/mpi_processing.py
+++ scripts/training/training_multiprocessing/mpi_processing.py
@@ -133,7 +133,9 @@ def main(config_loc, is_outer):
         os.environ["CUDA_VISIBLE_DEVICES"] = ""
         # tf_config.gpu_options.visible_device_list = ""
     else:
-        os.environ["CUDA_VISIBLE_DEVICES"] = str((rank+1)%2)
+        cuda_device = str((rank+1)%2)
+        os.environ["CUDA_VISIBLE_DEVICES"] = cuda_device
+        print("cuda_device", cuda_device)
         # tf_config.gpu_options.allow_growth = True
         
     gpus = tf.config.list_physical_devices('GPU')
@@ -153,10 +155,11 @@ def main(config_loc, is_outer):
     
     # Rank 0 initializes the program and runs the configuration loops
     if rank == 0:  
-         
+        
         # Get start time
         start_time_name = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         start_time = time.time()
+        start_perf = time.perf_counter()
         
         # Get the configurations
         configs = parse_training_configs(config_loc)
@@ -172,30 +175,31 @@ def main(config_loc, is_outer):
         
         # Get the tasks for each process
         tasks = split_tasks(configs, n_proc, is_outer)
-        print("tasks = ", tasks)
+        # tasks is a list of tuples
+        # where tuple has 4 elements
+        # 0: dictionary of configuration of hyperparameters
+        # 1: number of epochs
+        # 2: test_fold
+        # 3: validation_fold
+        print("len(tasks(top3) = ", tasks[:3])
+        print("tasks(top3) = ", tasks[:3])
+        
         # Listen for process messages while running
         exited = []
         while True:
-            subrank = comm.recv(
-                source=MPI.ANY_SOURCE
-            )
+            # it received rank from other processes
+            subrank = comm.recv(source=MPI.ANY_SOURCE)
             
             # Send task if the process is ready
             if tasks:
                 print(colored(f"Rank 0 is sending rank {subrank} their first task.", 'green'))
-                comm.send(
-                    tasks.pop(), 
-                    dest=subrank
-                )
+                comm.send(tasks.pop(), dest=subrank)
                 next_task_index += 1
                     
             # If no task remains, terminate process
             else:
                 print(colored(f"Rank 0 is terminating rank {subrank}, no tasks to give.", 'red'))
-                comm.send(
-                    False, 
-                    dest=subrank
-                )
+                comm.send(False, dest=subrank)
                 exited += [subrank]
                 
                 # Check if any processes are left, end this process if so
@@ -203,7 +207,8 @@ def main(config_loc, is_outer):
                     
                     # Get end time and print
                     print(colored(f"Rank 0 is printing the processing time.", 'red'))
-                    elapsed_time = time.time() - start_time
+                    elapsed_time = time.perf_counter() - start_perf
+
                     if not os.path.exists("../results/training_timings"):
                         os.makedirs("../results/training_timings")
                     outfile = f'_TIME_MPI_OUTER_{start_time_name}.txt' if is_outer else f'_TIME_MPI_INNER_{start_time_name}.txt'
@@ -217,8 +222,8 @@ def main(config_loc, is_outer):
         # tf.config.run_functions_eagerly(True)
         
         # Listen for the first task
-        print(colored(f'Rank {rank} is listening for process 0.', 'cyan'))
         comm.send(rank, dest=0)
+        print(colored(f'Rank {rank} is listening for process 0.', 'cyan'))
         task = comm.recv(source=0)
         
         # While there are tasks to run, train
